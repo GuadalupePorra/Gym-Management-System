274588dad5baef98a2cb19373baa498293bc5cd1
Guadalupe Porra <133470650+GuadalupePorra@users.noreply.github.com>
2025-08-16 19:08:06 -0300

    Update urls.py

--- django_api/urls.py
+++ django_api/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^.*$', FrontendAppView.as_view(), name="frontend"),
 ]
